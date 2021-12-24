7ef0eb31ff5221472e4b10b0cc233a39212262b9
Milan Štiglic <stiglic.work@gmail.com>
2021-12-24 19:54:37 +0100

    Added HEROKU vars

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@snotes-cluster.gzx7r.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@snotes-cluster.gzx7r.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://SuperAdmin:SuperSecretPassword123@snotes-cluster.gzx7r.mongodb.net/dev-db?retryWrites=true&w=majority`;
+const uri = process.env.MONGODB_URI;
 const db_connection = () => {
 	mongoose.connect(uri, {
 		useNewUrlParser: true,
